fe8b99baaa03f35b81b564260d9eb9e785723071
addadsanchez <dsanchez@adda.mx>
2016-06-21 00:34:32 -0500

    Initial Commit

---
+++ app/src/main/java/app/saverfid/com/saverfid/MainActivity.java
@@ -0,0 +1,25 @@
+package app.saverfid.com.saverfid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+        myRef.child("dato").setValue("Estefania es diseñadora.");
+
+
+
+
+    }
+}
